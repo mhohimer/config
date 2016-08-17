eef3b1548478600627cf720ef43bd8ede0f1610f
Michael Hohimer <mhohimer@cylance.com>
2016-08-17 11:32:52 -0700

    update

--- bin/mount_host.sh
+++ bin/mount_host.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+remote_host=mh-en-7m77s
+remote_path=proj
+remote_user=mhohimer
+local_path=~/host_proj
 
-sudo mount -t cifs //MH-EN-7M77S/proj /mnt/MH-EN-7M77S/proj -o username=mhohimer
+mkdir -p $local_path
+sudo mount -t cifs //$remote_host/$remote_path $local_path \
+    -o username=$remote_user,uid=$(id -u),gid=$(id -g),forceuid,forcegid
